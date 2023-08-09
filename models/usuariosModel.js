1c7d08c8c4fe463115703866ecc9100973eadcae
Isaac <isaakcovarrubias@gmail.com>
2023-08-09 10:44:27 -0400

    pequenos arreglos

--- models/usuariosModel.js
+++ models/usuariosModel.js
@@ -65,6 +65,12 @@ const Usuarios = database.define(
                 }
             }
 
+        },
+        Role:
+        {
+            type: DataTypes.ENUM['USUARIO', 'ADMINISTRADOR'],
+            allowNull: false,
+            defaultValue: 'USUARIO'
         }
         
     },
