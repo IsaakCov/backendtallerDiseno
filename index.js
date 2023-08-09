6a8d7e8f840934279f3fb0bfbb8fd0c629c60a98
Isaac <isaakcovarrubias@gmail.com>
2023-08-09 10:30:50 -0400

    tablas funcionando

--- index.js
+++ index.js
@@ -1,67 +1,53 @@
-// Instalamos y obtenemos express.
 const express = require('express');
 const app = express();
-// Importamos la base de datos para manipularla
-const conn = require('./services/database');
 
-// IMPORTAMOS LOS MODELOS DE TABLAS
-// Modelo de Usuarios
+// Importa la base de datos y modelos
+const conn = require('./services/database');
 const Usuarios = require('./models/usuariosModel.js');
 const FormularioDeConsultas = require('./models/formularioModel.js');
 const Pedidos = require('./models/pedidosModel.js');
 const Productos = require('./models/productosModel.js');
-const PedidoProductos = require('./models/pedidoProductosModel.js')
-//Creamos las conexiones de las tablas entre si
-// Definimos las asociaciones
+
+// Importa los controladores de rutas
+const createUsuario = require('./controllers/createUsuario');
+
+// Middleware para analizar el cuerpo de las solicitudes
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Asociaciones entre modelos
 Usuarios.hasMany(FormularioDeConsultas, { foreignKey: 'CorreoUsuario' });
 FormularioDeConsultas.belongsTo(Usuarios, { foreignKey: 'CorreoUsuario' });
 
 Usuarios.hasMany(Pedidos, { foreignKey: 'CorreoUsuario' });
 Pedidos.belongsTo(Usuarios, { foreignKey: 'CorreoUsuario' });
 
-Pedidos.belongsToMany(Productos, { through: 'PedidoProductos', foreignKey: 'idPedido' });
-Productos.belongsToMany(Pedidos, { through: 'PedidoProductos', foreignKey: 'idProducto' });
-
-
-// IMPORTAMOS LOS CONTROLADORES DE LAS RUTAS
-const createUsuario = require('./controllers/createUsuario')
-
-//Formateamos los requests para leerlos, esto analiza el cuerpo de las peticiones entrantes en:
-// Formato JSON
-app.use(express.json());
-// Formato listas o strings
-app.use(express.urlencoded({extended: true}));
+Pedidos.belongsToMany(Productos, { through: 'PedidoProductos' });
+Productos.belongsToMany(Pedidos, { through: 'PedidoProductos' });
 
 const port = 3001;
 
-//app.listen(port, console.log("Servidor ejecutandose correctamente"))
-
-// Generamos la conexion con la base de datos.
+// Conexión a la base de datos y sincronización de modelos
 const database = async () => {
     try {
         await conn.authenticate();
-        console.log('Base de datos conectada')
-        //Generamos la sincronizacion de nuestro modelo con la base de datos (compatibiliza la tabla, si no existe, la crea)
-        await Usuarios.sync({force: true});
-        await Productos.sync({force: true});
-        await Pedidos.sync({force: true});
-        await FormularioDeConsultas.sync({force: true});
-        
-        //await PedidoProductos.sync({force: true});
-    } 
-    catch (error) {
-        console.log('Algo salio mal con la conexion', error)
+        console.log('Base de datos conectada');
+        // Sincroniza los modelos con la base de datos (quitamos el force para no eliminar tablas existentes)
+        await Usuarios.sync();
+        await Productos.sync();
+        await Pedidos.sync();
+        await FormularioDeConsultas.sync();
+    } catch (error) {
+        console.log('Algo salió mal con la conexión:', error);
     }
-}
+};
 
-// Una vez generada la conexion, la llamaremos cada vez que se inicie el servidor
-
+// Inicia el servidor y sincroniza la base de datos al inicio
 app.listen(port, () => {
     database();
-    console.log("Servidor ejecutandose correctamente");
-})
+    console.log(`Servidor ejecutándose en el puerto ${port}`);
+});
 
-
-// Enrutamiento
-
+// Rutas
 app.post("/createUsuario", createUsuario);
+
--- models/pedidosModel.js
+++ models/pedidosModel.js
@@ -1,6 +1,4 @@
-// Importamos los tipos de datos de sequelize (que modela un sistema SQL) para definir nuestro modelo.
 const { DataTypes } = require('sequelize');
-// Importamos la base de datos para manipularla
 const database = require('../services/database.js');
 
 const Pedidos = database.define(
@@ -16,15 +14,6 @@ const Pedidos = database.define(
             type: DataTypes.STRING,
             allowNull: false,
         },
-        Productos: { 
-            type: DataTypes.ARRAY(DataTypes.JSON),
-            allowNull: false,
-            validate: {
-                notNull: {
-                    msg: 'Los productos son obligatorios.'
-                }
-            }
-        },
         TotalPedido: {
             type: DataTypes.FLOAT,
             allowNull: false,
@@ -37,16 +26,15 @@ const Pedidos = database.define(
         Estado: {
             type: DataTypes.ENUM,
             values: ['PENDIENTE', 'CONFIRMADO', 'ENVIADO', 'ENTREGADO', 'ANULADO'],
-            primaryKey: true,
             allowNull: false,
             defaultValue: 'PENDIENTE'
-        }
+        },
     },
     {
         timestamps: true,
         createdAt: true,
         updatedAt: true
     }
-)
+);
 
 module.exports = Pedidos;
